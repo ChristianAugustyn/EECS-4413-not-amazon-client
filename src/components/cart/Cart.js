eeb4a6a9ae6049a86d837624c05ded3246df2f20
Christian <chrisaug@rogers.com>
2021-03-31 15:20:12 -0400

    fresh build with new dependencies

--- src/components/cart/Cart.js
+++ src/components/cart/Cart.js
@@ -4,15 +4,21 @@ import { connect } from "react-redux";
 import { increment, decrement, addToCart } from "../../state/actions";
 
 const Cart = ({ count, cart, addToCart }) => {
+
+  console.log(cart)
+
   return (
     <div className="cart">
       <h2>Welcome to the Cart page</h2>
-      {cart.length === 0 ? 'There are no items in your cart' : `there are ${cart.length} item in your cart`}
+      {!!cart && cart.length === 0
+        ? "There are no items in your cart"
+        : `there are ${cart.length} item in your cart`}
     </div>
   );
 };
 
-const mapStateToProps = (state) => { //takes the values from the cart
+const mapStateToProps = (state) => {
+  //takes the values from the cart
   return {
     count: state.count,
     cart: state.cart,
