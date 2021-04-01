85ab8b1b32578cea5f9e1982cae5bdfee20e8614
SamuelOn <samuelon3@gmail.com>
2021-04-01 00:02:35 -0400

    Cart almost done. quantity buttons finished.

--- src/components/cart/Cart.js
+++ src/components/cart/Cart.js
@@ -1,18 +1,64 @@
-import React from "react";
-import "./Cart.css";
-import { connect } from "react-redux";
-import { increment, decrement, addToCart } from "../../state/actions";
+import React from 'react';
+import './Cart.css';
+import { connect } from 'react-redux';
+import {
+  increment,
+  decrement,
+  addToCart,
+  quantityAdd,
+  quantitySub
+} from '../../state/actions';
+import { Table, Button } from 'react-bootstrap';
 
-const Cart = ({ count, cart, addToCart }) => {
+const Cart = ({ count, cart, addToCart, quantityAdd, quantitySub }) => {
+  console.log(cart);
 
-  console.log(cart)
+  const itemList = cart.map((item) => (
+    <tr key={item.bid}>
+      <td>
+        <img alt="book" src={item.image}></img>
+      </td>
+      <td>{item.title}</td>
+      <td>{item.category}</td>
+      <td>{item.price}</td>
+      <td>{item.quantity}</td>
+      <td>
+        <Button variant="light" onClick={() => quantityAdd(item.bid)}>
+          +
+        </Button>
+        <Button variant="light" onClick={() => quantitySub(item.bid)}>
+          -
+        </Button>
+      </td>
+      <td>
+        ${(Math.round(item.price * item.quantity * 100) / 100).toFixed(2)}
+      </td>
+    </tr>
+  ));
+
+  const totalCost = 0;
 
   return (
     <div className="cart">
       <h2>Welcome to the Cart page</h2>
       {!!cart && cart.length === 0
-        ? "There are no items in your cart"
+        ? 'There are no items in your cart'
         : `there are ${cart.length} item in your cart`}
+      <Table responsive>
+        <thead>
+          <tr>
+            <th>Product</th>
+            <th>Title</th>
+            <th>Category</th>
+            <th>Price</th>
+            <th>Quantity</th>
+            <th></th>
+            <th>Total</th>
+          </tr>
+        </thead>
+        <tbody>{itemList}</tbody>
+      </Table>
+      <Button>CheckOut</Button>
     </div>
   );
 };
@@ -21,7 +67,7 @@ const mapStateToProps = (state) => {
   //takes the values from the cart
   return {
     count: state.count,
-    cart: state.cart,
+    cart: state.cart
   };
 };
 
@@ -30,6 +76,8 @@ const mapDispatchToProps = (dispatch) => {
     increment: () => dispatch(increment()),
     decrement: () => dispatch(decrement()),
     addToCart: (item) => dispatch(addToCart(item)),
+    quantityAdd: (id) => dispatch(quantityAdd(id)),
+    quantitySub: (id) => dispatch(quantitySub(id))
   };
 };
 
--- src/state/actions.js
+++ src/state/actions.js
@@ -1,26 +1,38 @@
 const increment = () => {
-    return {
-        type: 'INC',
-        value: 1
-    }
-}
+  return {
+    type: 'INC',
+    value: 1
+  };
+};
 
 const decrement = () => {
-    return {
-        type: 'DEC',
-        value: 1
-    }
-}
+  return {
+    type: 'DEC',
+    value: 1
+  };
+};
 
-const addToCart = item => {
-    return {
-        type: 'ADD_ITEM',
-        value: item
-    }
-}
+const addToCart = (item) => {
+  return {
+    type: 'ADD_ITEM',
+    value: item
+  };
+};
 
-export {
-    increment,
-    decrement,
-    addToCart
-}
+const quantityAdd = (id) => {
+  console.log('im in quantity');
+  return {
+    type: 'QUANTITY_ADD',
+    value: id
+  };
+};
+
+const quantitySub = (id) => {
+  console.log('im in quantity Sub');
+  return {
+    type: 'QUANTITY_SUB',
+    value: id
+  };
+};
+
+export { increment, decrement, addToCart, quantityAdd, quantitySub };
