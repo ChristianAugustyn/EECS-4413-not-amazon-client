2a3aba6d1040d5c10c73e52cc8aea8c9b8319030
Christian <chrisaug@rogers.com>
2021-04-07 14:05:58 -0400

    added logo

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -4,51 +4,63 @@ import { connect } from "react-redux";
 import { increment, decrement } from "../../state/actions";
 import { Container, Row, Col, Jumbotron } from "react-bootstrap";
 import BookCard from "../book-card/BookCard";
-import {useSpring, animated} from 'react-spring'
+import logo from "../../static/logo512.png";
 import axios from "axios";
 
 const App = ({ count, increment, decrement }) => {
+    const [isLoading, setIsLoading] = useState(true);
+    const [books, setBooks] = useState([]);
 
-  const animation = useSpring({opacity: 1, from: {opacity: 0}})
+    useEffect(() => {
+        var config = {
+            method: "get",
+            url:
+                "https://eecs-4413-notamazon.mybluemix.net/rest/books/allbooks",
+            headers: {},
+        };
 
-  const [isLoading, setIsLoading] = useState(true);
-  const [books, setBooks] = useState([]);
+        axios(config)
+            .then((res) => {
+                setBooks(res.data.allBooks);
+                setTimeout(() => {
+                    setIsLoading(false);
+                }, 200);
+            })
+            .catch((error) => {
+                console.log(error);
+            });
+    }, []);
 
-  useEffect(() => {
-    var config = {
-      method: "get",
-      url: "https://eecs-4413-notamazon.mybluemix.net/rest/books/allbooks",
-      headers: {},
-    };
-
-    axios(config)
-      .then((res) => {
-        setBooks(res.data.allBooks);
-        setTimeout(() => {
-          setIsLoading(false)
-        }, 200)
-      })
-      .catch((error) => {
-        console.log(error);
-      });
-  }, []);
-
-  return (
-    <div className="App">
-      <Jumbotron>
-        <animated.h1 style={animation}>Welcome to notAmazon</animated.h1>
-        <p>The brick-and-mortar Mom & Pop book shop</p>
-      </Jumbotron>
-      <Container>
-        <Row>
-          {!isLoading && books.map((book) => (
-            <Col sm={6} md={6} lg={4}>
-              <BookCard book={book} />
-            </Col>
-          ))}
-        </Row>
-      </Container>
-      {/* <div>
+    return (
+        <div className="App">
+            <Jumbotron>
+                <Container>
+                    <Row>
+                        <Col sm={12} lg={6}>
+                            <div className='header'>
+                                <h1>
+                                    Welcome to notAmazon
+                                </h1>
+                                <p>The brick-and-mortar Mom & Pop book shop</p>
+                            </div>
+                        </Col>
+                        <Col lg={6} className='logo d-sm-none d-lg-block'>
+                            <img src={logo} />
+                        </Col>
+                    </Row>
+                </Container>
+            </Jumbotron>
+            <Container>
+                <Row>
+                    {!isLoading &&
+                        books.map((book) => (
+                            <Col sm={6} md={6} lg={4}>
+                                <BookCard book={book} />
+                            </Col>
+                        ))}
+                </Row>
+            </Container>
+            {/* <div>
           <h3>Redux State</h3>
           <button onClick={() => decrement()}>
             decrement
@@ -58,21 +70,21 @@ const App = ({ count, increment, decrement }) => {
             increment
           </button>
         </div> */}
-    </div>
-  );
+        </div>
+    );
 };
 
 const mapStateToProps = (state) => {
-  return {
-    count: state.count,
-  };
+    return {
+        count: state.count,
+    };
 };
 
 const mapDispatchToProps = (dispatch) => {
-  return {
-    increment: () => dispatch(increment()),
-    decrement: () => dispatch(decrement()),
-  };
+    return {
+        increment: () => dispatch(increment()),
+        decrement: () => dispatch(decrement()),
+    };
 };
 
 export default connect(mapStateToProps, mapDispatchToProps)(App);
