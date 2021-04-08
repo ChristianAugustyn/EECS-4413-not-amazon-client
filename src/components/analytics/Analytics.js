c0098e975a70ab7378b3bf449d18e20033bc912e
samuel <samuelon3@gmail.com>
2021-04-08 03:47:31 -0400

    put in both query calls

--- src/components/analytics/Analytics.js
+++ src/components/analytics/Analytics.js
@@ -1,20 +1,64 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { Table, Form } from 'react-bootstrap';
 import ar from './Analytics.module.css';
+import axios from 'axios';
 import { connect } from 'react-redux';
 
 const Analytics = () => {
   const [state, setState] = useState({
     dateRange: ''
   });
+  const [books, setBooks] = useState([]);
+  const [userSpentBooks, setuserSpentBooks] = useState([]);
 
-  const topTenBooks = (
+  useEffect(() => {
+    // GET request using fetch inside useEffect React hook
+    var axios = require('axios');
+
+    var config = {
+      method: 'get',
+      url: 'http://localhost:8080/EECS-4413-notAmazon/rest/admin/topten',
+      headers: {}
+    };
+
+    axios(config)
+      .then(function (response) {
+        console.log(JSON.stringify(response.data));
+        setBooks(response.data.topTenBooks.reverse());
+        console.log(books);
+      })
+      .catch(function (error) {
+        console.log(error);
+      });
+
+    var config = {
+      method: 'get',
+      url: 'http://localhost:8080/EECS-4413-notAmazon/rest/admin/userspentzip',
+      headers: {}
+    };
+
+    axios(config)
+      .then(function (response) {
+        console.log(JSON.stringify(response.data));
+        setuserSpentBooks(response.data.userSpentZip);
+      })
+      .catch(function (error) {
+        console.log(error);
+      });
+
+    // empty dependency array means this effect will only run once (like componentDidMount in classes)
+  }, []);
+
+  // topTenBooks.map((book) =>
+  const topTenBooksList = books.slice(0, 10).map((book) => (
     //generate books w/ axio call
     <tr>
-      <td>1</td>
-      <td> Alice in Wonderland</td>
+      <td>{book.count}</td>
+      <td>{book.title}</td>
     </tr>
-  );
+  ));
+
+  // Update the document title using the browser API    document.title = `You clicked ${c
 
   const numBooksSold = (
     //generate books w/ axio call
@@ -24,14 +68,14 @@ const Analytics = () => {
     </tr>
   );
 
-  const anonReport = (
+  const anonReport = userSpentBooks.slice(0, 10).map((USBook) => (
     //generate statistic w/ axio call
     <tr>
-      <td>****</td>
-      <td>ads asd </td>
-      <td>$10 </td>
+      <td>{USBook.userid}</td>
+      <td>{USBook.zip}</td>
+      <td>{USBook.total}</td>
     </tr>
-  );
+  ));
 
   const handleChange = (e) => {
     const { name, value } = e.target;
@@ -55,7 +99,7 @@ const Analytics = () => {
         <option> January</option>
         <option> Feburary</option>
       </Form.Control>
-
+      <br />
       <Table striped bordered hover>
         <thead>
           <tr>
@@ -74,7 +118,7 @@ const Analytics = () => {
             <th>Book Titles</th>
           </tr>
         </thead>
-        <tbody>{topTenBooks}</tbody>
+        <tbody>{topTenBooksList}</tbody>
       </Table>
       <br />
       <h2> User Buying Statistic</h2>
